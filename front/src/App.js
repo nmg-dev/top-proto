d0909212daf8b76833e2858d5cc1805976bdf967
yg.song@nextmediagroup.co.kr <insaint03@gmail.com>
2018-12-06 21:00:37 +0900

    barebone

---
+++ front/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Paper } from '@material-ui/core';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Paper>
+          
+        </Paper>
+      </div>
+    );
+  }
+}
+
+export default App;

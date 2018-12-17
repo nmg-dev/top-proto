ec67ab722754ee2b97d22d9bde945bbf2952b9ad
yg.song@nextmediagroup.co.kr <insaint03@gmail.com>
2018-12-17 14:01:54 +0900

    front to view

--- front/src/appManage.js
+++ front/src/appManage.js
@@ -10,24 +10,65 @@ class AppManage extends Component {
 		super(props);
 	}
 
+	onFileSelect(ev) {
+		console.log(ev.target.files);
+
+		let reader = new FileReader();
+		reader.onload = this.onFileLoad.bind(this);
+		let filesToRead = ev.target.files;
+		if(filesToRead && 0<filesToRead.length) {
+			for(let fk in filesToRead) {
+				if(filesToRead[fk] instanceof File) {
+					console.log(filesToRead[fk]);
+					reader.readAsText(filesToRead[fk]);
+				}
+			}
+			
+		}
+	}
+
+	onFileLoad(ev) {
+		let lines = ev.target.result.split("\n")
+		let values = [];
+		let columns = [];
+		lines.forEach((line) => {
+			let tokens = line.split(/\t/g);
+			if(columns.length<=0) {
+				columns = tokens;
+			} else {
+				values.push(tokens);
+			}
+		});
+		console.log(values);
+	}
+
 	render() {
 		return (
 			<Grid container>
-				<Card>
-					<CardHeader 
-						avatar={<Icon>plus</Icon>}
-						title="NEW Campaign"
-					/>
-					<CardContent>
-						<TextField label="campaign title" />
-						<TextField label="campaign account" />
-					</CardContent>
-					<CardActions>
-						<Button>SUBMIT</Button>
-						<Button>UPLOAD</Button>
-						<Button>CANCEL</Button>
-					</CardActions>
-				</Card>
+				<Grid item xs={12}>
+					<Card>
+						<CardContent>
+							<div>
+							<input type="file" onChange={this.onFileSelect.bind(this)} />
+							</div>
+							<div>
+								</div>
+						</CardContent>
+						<CardActions>
+							<Button>Save</Button>
+						</CardActions>
+					</Card>
+				</Grid>
+				<Grid item xs={12}>
+					<Card>
+						<CardHeader>
+							Campaigns
+						</CardHeader>
+						<CardContent>
+
+						</CardContent>
+					</Card>
+				</Grid>
 			</Grid>
 		);
 	}
